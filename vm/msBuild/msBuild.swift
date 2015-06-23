2df0c2445b65ad461a2513f1232020dd52d69c74
Dennis Pettersson <mail@dennispettersson.se>
2015-06-23 02:15:28 +0200

    It works!

--- vm/msBuild/msBuild.swift
+++ vm/msBuild/msBuild.swift
@@ -8,19 +8,46 @@
 
 import Foundation
 
+internal struct Paths {
+  
+  struct File {
+    static let script = "build.bat"
+    static let log = "out.log"
+  }
+  
+  struct Windows {
+    static let temp = "C:\\temp"
+    static let log = "\(temp)\\\(Paths.File.log)"
+    static let script = "\(temp)\\\(Paths.File.script)"
+    static let cmd = "C:\\WINDOWS\\system32\\cmd.exe"
+  }
+  
+  struct OSX {
+    static let temp = "~/temp".stringByExpandingTildeInPath
+    static let log = "\(temp)/\(Paths.File.log)"
+    static let script = "\(temp)/\(Paths.File.script)"
+  }
+}
+
 internal enum Response: String {
   
   case OK = "OK"
   case FileExists = "File exists"
+  case FileDoesNotExist = "File does not exist"
   case DirectoryExists = "Directory exists"
+  case DirectoryDoesNotExist = "Directory does not exist"
   case Credentials = "Username/Password incorrect"
   case Unknown = "Something went wrong"
   
   init(_ response: String, _ error: String) {
     if response.contains("file exists") {
       self = .FileExists
+    } else if response.contains("file does not") || error.contains("file does not") {
+      self = .FileDoesNotExist
     } else if response.contains("directory exists") {
       self = .DirectoryExists
+    } else if response.contains("directory does not") || error.contains("directory does not") {
+      self = .DirectoryDoesNotExist
     } else if response.contains("unknown") || error.contains("unkown") {
       self = .Credentials
     } else if error == "" {
@@ -54,20 +81,16 @@ public class MSBuild {
 public extension MSBuild {
   
   func run() {
-    checkIfExists(selected.options.solution.value.removeQuotations.windowsEcaping, .FileExists)
-    checkIfExists(selected.options.msbuild.value.removeQuotations.windowsEcaping, .FileExists)
-    checkIfExists(Paths.Windows.temp, .DirectoryExists) 
+//    checkIfExists(selected.options.solution.value.removeQuotations.windowsEcaping, .FileExists)
+//    checkIfExists(selected.options.msbuild.value.removeQuotations.windowsEcaping, .FileExists)
+//    checkIfExists(Paths.Windows.temp, .DirectoryExists)
+    
+    createBuildScript()
   }
 }
 
 // MARK: Private
 private extension MSBuild {
-  
-  struct Paths {
-    struct Windows {
-      static let temp = "C:\\temp"
-    }
-  }
   
   var auth: [String] {
     get {
@@ -89,7 +112,7 @@ private extension MSBuild {
     return (status == checkAgainst, status)
   }
 
-  func checkIfExists(path: String, _ res: Response) -> Void {
+  func checkIfExists(path: String, _ res: Response, haltOnError: Bool = true) -> Bool {
     var type = res == .FileExists ? "file" : "directory"
     let (ok, status) = vmWareRequest([
       "\(type)ExistsInGuest",
@@ -97,14 +120,92 @@ private extension MSBuild {
       path
     ], res)
     
-    if ok == false {
+    if haltOnError && ok == false {
       halt(status.rawValue)
     }
+    
+    return ok
   }
   
   func createBuildScript() {
+    var file: String = "\r\n".join([
+      "c:\\",
+      "CALL " +
+        "\"\(selected.options.msbuild.value.removeQuotations.windowsEcaping)\" " +
+        "\"\(selected.options.solution.value.removeQuotations.windowsEcaping)\" " +
+        "\(selected.options.task.value) " +
+        "\(selected.options.property.value)" +
+        " >> " +
+        "\"\(Paths.Windows.log)\"" +
+      ""
+    ])
+    
+    var error: NSError?
+    if fm.fileExistsAtPath(Paths.OSX.temp) == false {
+      fm.createDirectoryAtPath(Paths.OSX.temp, withIntermediateDirectories: true, attributes: nil, error: &error)
+    }
+    if error != nil {
+      halt("could not create temp folder")
+    }
+
+    if checkIfExists(Paths.Windows.script, .FileExists, haltOnError: false) == true {
+      vmWareRequest(["deleteFileInGuest", selected.id, Paths.Windows.script])
+    }
+    
+    if checkIfExists(Paths.Windows.log, .FileExists, haltOnError: false) == true {
+      vmWareRequest(["deleteFileInGuest", selected.id, Paths.Windows.log])
+    }
+    
+    let data = (file as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+    if fm.createFileAtPath(Paths.OSX.script, contents: data, attributes: nil) == false {
+      halt("could not create script file")
+    }
+    
+    sendBuildScript()
   }
   
   func sendBuildScript() {
+    vmWareRequest(["CopyFileFromHostToGuest", selected.id, Paths.OSX.script, Paths.Windows.script])
+    
+    if checkIfExists(Paths.Windows.script, .FileExists, haltOnError: false) == false {
+      halt("script not present in guest")
+    }
+    
+    println("\(ASCIIColor.Bold.green)Running script\(ASCIIColor.reset)")
+    vmWareRequest([
+      "runProgramInGuest",
+      selected.id,
+      Paths.Windows.cmd,
+      "/c \"\(Paths.Windows.script)\""
+    ])
+    
+    readLogs()
+  }
+  
+  func readLogs() {
+    if checkIfExists(Paths.Windows.log, .FileExists, haltOnError: false) == false {
+      halt("logs where not created!")
+    }
+    vmWareRequest(["CopyFileFromGuestToHost", self.selected.id, Paths.Windows.log, Paths.OSX.log])
+    let success: Bool
+    let file: String
+    
+    if let data = fm.contentsAtPath(Paths.OSX.log),
+      let dataAsString = NSString(data: data, encoding: NSUTF8StringEncoding) as? String {
+        file = dataAsString
+        success = file.contains("Build succeeded.")
+    } else {
+      file = ""
+      success = false
+    }
+    
+    if success {
+      var index = file.rangeOfString("Build succeeded.")!.startIndex
+      let part = file.substringFromIndex(index)
+      println(part)
+    } else {
+      println(file)
+      println("build not successful")
+    }
   }
 }
--- vm/utils/String.swift
+++ vm/utils/String.swift
@@ -30,6 +30,10 @@ extension String {
     return "\\".join(split(self.stringByReplacingOccurrencesOfString("\\\\", withString: "\\"), maxSplit: 1, allowEmptySlices: true){ $0 == "\\" })
   }
   
+  func substringFromIndex(index:Int) -> String {
+    return self.substringFromIndex(advance(self.startIndex, index))
+  }
+  
   var removeQuotations: String {
     var modifiedString = self
     if self.hasPrefix("\"") {
